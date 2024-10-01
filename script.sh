ff6739a162802ca8dce91fdf4c98b9206f77c788
aman <mnkapezov@gmail.com>
2024-10-01 12:38:11 +0500

    the join_date has been replaced to id

--- script.sh
+++ script.sh
@@ -3,9 +3,9 @@
 filename=$2
 
 if [ -e "$filename.json" ]; then
-        jq '.users[] | select(.id == 1) | .join_date' "$filename.json"
+        jq '.users[] | select(.id == 1) | .id' "$filename.json"
 
 else
         curl -s "$1" | jq . > "$filename.json"
-        jq '.users[] | select(.id == 1) | .join_date' "$filename.json"
+        jq '.users[] | select(.id == 1) | .id' "$filename.json"
 fi
